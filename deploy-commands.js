dbbb15d703fc405a32fa98fecbb2785983d07f38
Under <jorgeolmo.I@gmail.com>
2022-04-14 17:06:42 +0200

    Registro de comandos y primera version

---
+++ deploy-commands.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const commands = [
+    new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
+    new SlashCommandBuilder().setName('server').setDescription('server!'),
+    new SlashCommandBuilder().setName('user').setDescription('user!')
+].map(command => command.toJSON())
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN)
+
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID ,process.env.GUILD_ID), { body: commands }).then(() => {
+    console.log('Successfully deployed commands!')
+}).catch(console.error)
--- index.js
+++ index.js
@@ -9,4 +9,19 @@ client.once('ready', () => {
     console.log('Ready!')
 })
 
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isCommand) return
+
+    const { commandName } = interaction
+
+    if(commandName === 'ping') {
+        interaction.reply('Pong!')
+    } else if (commandName === 'server') {
+        interaction.reply(`Server: ${interaction.guild.name} \nTotal members: ${interaction.guild.memberCount}`)
+    } else if (commandName === 'user') {
+        interaction.reply(`User info. \nTag: ${interaction.user.tag}\nID: ${interaction.user.id}`)
+    }
+
+})
+
 client.login(process.env.TOKEN)
