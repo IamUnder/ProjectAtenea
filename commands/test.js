614c4bfe82faa0c9c7262044babdc3aa00a51127
Under <jorgeolmo.I@gmail.com>
2022-05-03 20:54:16 +0200

    Arreglo de comandos

--- commands/test.js
+++ commands/test.js
@@ -1,10 +1,16 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const msg = "test!"
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('test')
         .setDescription('test!'),
     async execute(client, interaction) {
-        await interaction.reply('Jiji testing!')
+        if (interaction.content === undefined) {
+            return interaction.reply(msg)
+        } else {
+            const channel = client.channels.cache.get(interaction.channelId)
+            await channel.send(msg)
+        }
     }
 }
